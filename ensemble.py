af5396500c1e157425c15a12aef9f55678629e8d
qin you <mg20150006@smail.nju.edu.cn>
2021-11-23 21:02:29 +0800

    torch.tensor([t1,t2])-->2 dims

--- ensemble.py
+++ ensemble.py
@@ -67,7 +67,7 @@ class Ensemble_utils:
                 if gate_pretrain_steps < self.gate_pretrain_threshold: epsilon = 1
                 if torch.rand(1).item() < 1-epsilon:
                     scores = [gate(torch.cat((state.squeeze()[:goal_dim], goal.squeeze()))).squeeze() for gate in [gates[i]['gate_net'] for i in range(self.n_ensemble)]]
-                    ind = torch.tensor(scores).argmax().item()
+                    ind = torch.tensor(scores).squeeze().argmax().item()
                 else:
                     ind = torch.randint(0,high=self.n_ensemble, size=(1,)).item()
                 self.cur_agent_ind = ind
